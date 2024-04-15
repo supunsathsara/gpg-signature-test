442d3bc2fe7b25d9c3079deb2ae1b499885ad3dc
“supunsathsara” <savindusupunsathsara@gmail.com>
2024-04-16 02:26:48 +0530

    Adding a signed commit from terminal ✨

--- world.ts
+++ world.ts
@@ -2,3 +2,4 @@ console.log("Hi from MacBook Pro M1!")
 console.log("After a long time, I'm back to coding!")
 
 console.log("Checking if everything works ok after setting up powerlevel10k on MBP")
+console.log("committing with gpg key from terminal")
