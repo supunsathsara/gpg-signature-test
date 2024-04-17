5dcb1bfd48a06a8aab2c0f9369bb340e009cada6
Savindu Supun <contact@supunsathsara.com>
2024-04-18 02:05:16 +0530

    Update world.ts :sparkles:

--- world.ts
+++ world.ts
@@ -3,3 +3,5 @@ console.log("After a long time, I'm back to coding!")
 
 console.log("Checking if everything works ok after setting up powerlevel10k on MBP")
 console.log("committing with gpg key from terminal")
+
+console.log("Committing from Github Desktop")
